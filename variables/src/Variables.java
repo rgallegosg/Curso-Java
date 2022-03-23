25bed0da003f5d2b0164441dbe332938ab50e19b
Ricardo Gallegos <gallegos.rickgalle@gmail.com>
2022-03-23 14:48:35 -0600

    variables

---
+++ variables/src/Variables.java
@@ -0,0 +1,19 @@
+public class Variables {
+    public static void main(String[] args) {
+        //String Objet
+        String greet = "Hello World from Java";
+        System.out.println(greet);
+        //soutv take the last variable
+        System.out.println("greet.toUpperCase() = " + greet.toUpperCase());
+
+        //Int variable
+        int number = 10;
+        System.out.println("number = " + number);
+
+        boolean value = false;
+
+        if (value) {
+            System.out.println("Is true");
+        }
+    }
+}
